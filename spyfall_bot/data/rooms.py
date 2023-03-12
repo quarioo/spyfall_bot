c364d454c3281d8df7055caef328672730922ff7
quarioo <kirillzubrilin.zzz@gmail.com>
2023-03-12 21:58:15 +0300

    Create db using sqlalchemy

---
+++ spyfall_bot/data/rooms.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+
+class Room(SqlAlchemyBase):
+    __tablename__ = 'rooms'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String,
+                             nullable=False, unique=True)
+    started_at = sqlalchemy.Column(sqlalchemy.DateTime,
+                                   nullable=False)
+    time = sqlalchemy.Column(sqlalchemy.Time, nullable=False)
+    max_users = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
