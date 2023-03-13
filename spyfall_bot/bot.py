aac7c45dc877bb7d75fe31559717533f009bcbca
quarioo <kirillzubrilin.zzz@gmail.com>
2023-03-13 20:46:11 +0300

    Start creating /createroom

--- spyfall_bot/bot.py
+++ spyfall_bot/bot.py
@@ -19,10 +19,28 @@ if not TELEGRAM_BOT_TOKEN:
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     effective_chat = update.effective_chat
     if effective_chat is None:
-        logger.warning('effective_chat is None')
+        logger.warning('effective_chat is None in /start')
 
     await context.bot.send_message(chat_id=effective_chat.id,
                                    text=message_texts.GREETINGS)
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if effective_chat is None:
+        logger.warning('effective_chat is None in /help')
+
+    await context.bot.send_message(chat_id=effective_chat.id,
+                                   text=message_texts.HELP)
+
+
+async def createroom(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if effective_chat is None:
+        logger.warning('effective_chat is None in /createroom')
+
+    await context.bot.send_message(chat_id=effective_chat.id,
+                                   text=message_texts.CREATING_GAME_1)
 
 
 if __name__ == '__main__':
@@ -30,5 +48,11 @@ if __name__ == '__main__':
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    help_handler = CommandHandler('help', help)
+    application.add_handler(help_handler)
+
+    createroom_handler = CommandHandler('createroom', createroom)
+    application.add_handler(createroom_handler)
 
     application.run_polling()
